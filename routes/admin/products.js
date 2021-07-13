acdb083a2afa6d61a8baaa56b990b2da04d43092
hproctor021 <hproctor021@gmail.com>
2021-07-13 14:03:22 -0500

    validations, image upload handling, newproducts form & styling

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -1,7 +1,14 @@
 const express = require('express');
-const router = express.Router();
-const ProductsRepo = require('../../repositories/products');
+const { validationResult } = require('express-validator');
+const multer = require('multer');
 
+const ProductsRepo = require('../../repositories/products');
+const productsNewTemplate = require('../../views/admin/products/new');
+const { requireTitle, requirePrice } = require('./validators');
+
+const router = express.Router();
+const upload = multer({ storage: multer.memoryStorage() });
+// middleware function
 
 router.get('/admin/products', (req, res) => {
 
@@ -9,7 +16,24 @@ router.get('/admin/products', (req, res) => {
 
 
 router.get('/admin/products/new', (req, res) => {
-    
+    res.send(productsNewTemplate({}));
 });
+
+router.post('/admin/products/new', upload.single('image'), [ requireTitle, requirePrice ], async (req, res) => {
+    // upload.single('image') is from multer documentation - image is the name of the 
+    // property where the image is in the form to be submitted
+
+    const errors = validationResult(req);
+
+    if( !errors.isEmpty() ){
+        return res.send(productsNewTemplate({ errors }));
+    }
+
+    const image = req.file.buffer.toString('base64');
+    const { title, price } = req.body;
+    await ProductsRepo.create({ title, price, image });
+    
+    res.send('New product submitted')
+})
 
 module.exports = router;
