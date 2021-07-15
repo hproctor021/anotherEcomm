823606a543cb7a1cc46d7aea56343f6d7dcf57f2
hproctor021 <hproctor021@gmail.com>
2021-07-15 13:27:26 -0500

    user session auth added

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -6,24 +6,30 @@ const productsNewTemplate = require('../../views/admin/products/new');
 const productsIndexTemplate = require('../../views/admin/products/index');
 
 const { requireTitle, requirePrice } = require('./validators');
-const { handleErrors } = require('./middlewares');
+const { handleErrors, requireAuth } = require('./middlewares');
 // const products = require('../../repositories/products');
 
 const router = express.Router();
 const upload = multer({ storage: multer.memoryStorage() });
 // middleware function
 
-router.get('/admin/products', async (req, res) => {
+router.get('/admin/products', requireAuth, async (req, res) => {
     const products = await productsRepo.getAll();
     res.send(productsIndexTemplate({ products }));
 });
 
 
-router.get('/admin/products/new', (req, res) => {
+router.get('/admin/products/new', requireAuth, (req, res) => {
     res.send(productsNewTemplate({}));
 });
 
-router.post('/admin/products/new', upload.single('image'), [ requireTitle, requirePrice ], handleErrors(productsNewTemplate), async (req, res) => {
+router.post(
+    '/admin/products/new', 
+    requireAuth,
+    upload.single('image'), 
+    [ requireTitle, requirePrice ], 
+    handleErrors(productsNewTemplate), 
+    async (req, res) => {
     // upload.single('image') is from multer documentation - image is the name of the 
     // property where the image is in the form to be submitted
 
